2a162e1e8b0fed94b7424e7952d4493d831e90ba
leesemin <leesemin222@gmail.com>
2023-05-31 17:07:55 +0900

    컴포넌트 태그 사이의 children값 화면에 띄우기

--- rePrac/src/MyComponent.js
+++ rePrac/src/MyComponent.js
@@ -1,5 +1,10 @@
 const MyComponent = (props) => {
-  return <div>안녕 만나서 반가워 내 이름은 {props.name} 이야!</div>;
+  return (
+    <div>
+      안녕 만나서 반가워 내 이름은 {props.name} 이야! <br />
+      children 값은 {props.children} 이야!
+    </div>
+  );
 };
 
 MyComponent.defaultProps = {
