43ad976e2331b3ce63fa0fb0ab73159f33d36d3e
leesemin <leesemin222@gmail.com>
2023-05-31 15:14:59 +0900

    미리 선언한 style을 불러와 인라인으로 설정

--- rePrac/src/App.js
+++ rePrac/src/App.js
@@ -1,8 +1,15 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 function App() {
-  const name = undefined;
-  return <div>{name || "리액트"}</div>;
+  const name = "리액트";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: "48px",
+    fontWeight: "bold",
+    padding: 16, // 단위 생략시 px로 자동 지정됨
+  };
+  return <div style={style}>{name}</div>;
 }
 
 export default App;
