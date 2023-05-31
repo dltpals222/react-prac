09bd85479a03729cbf6fc04442d3bbaf78f7e86c
leesemin <leesemin222@gmail.com>
2023-05-31 14:53:08 +0900

    챕터 1

--- rePrac/src/App.js
+++ rePrac/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 
 function App() {
-  const [count, setCount] = React.useState(0);
-  const incremenetCount = () => {
-    setCount(count + 1);
-  };
   return (
     <div>
-      <h1>Counter App</h1>
-      <p>Count : {count}</p>
-      <button onClick={incremenetCount}>Incremenet</button>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>
     </div>
   );
 }
