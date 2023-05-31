c1d5d1fbde6a7af5e4c61cf87bfbf79d714cbf4c
leesemin <leesemin222@gmail.com>
2023-05-31 15:32:28 +0900

    하나만으로 사용 가능한 태그들을 사용할 시 에러가 날 수도 있다.

--- rePrac/src/App.js
+++ rePrac/src/App.js
@@ -2,7 +2,8 @@ import "./App.css"; /* import 하지 않으면 적용 안됨 */
 
 function App() {
   const name = "리액트";
-  return <div className="react">{name}</div>;
+  return (<div className="react">{name}</div>
+  <input>);
 } /* 원래 HTML은 class 가 맞지만 리액트에서는 className이 맞다 */
 
 export default App;
