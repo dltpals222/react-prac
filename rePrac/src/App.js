d2e9dcef3ef4b333db76f4bf462562b9dfaf62bd
leesemin <leesemin222@gmail.com>
2023-05-31 15:16:15 +0900

    미리 선언하지 않고 직접 입력도 가능(이중 중괄호가 됨)

--- rePrac/src/App.js
+++ rePrac/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 
 function App() {
   const name = "리액트";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: "48px",
-    fontWeight: "bold",
-    padding: 16, // 단위 생략시 px로 자동 지정됨
-  };
-  return <div style={style}>{name}</div>;
+
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "aqua",
+        fontSize: "48px",
+        fontWeight: "bold",
+        padding: 16, // 단위 생략시 px로 자동 지정됨
+      }}
+    >
+      {name}
+    </div>
+  );
 }
 
 export default App;
